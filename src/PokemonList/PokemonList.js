b69626a50bc4d7a811311be813ecccc8983e03bf
JorgeRinaldi1995 <jorge.rinaldi.1995@gmail.com>
2024-05-30 21:31:07 -0300

    layout

--- src/PokemonList/PokemonList.js
+++ src/PokemonList/PokemonList.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './style.scss'
+import './style.scss';
 
 function PokemonList() {
     const [pokemons, setPokemons] = useState([]);
-    const [nextPage, setNextPage] = useState('https://pokeapi.co/api/v2/pokemon?offset=0&limit=36');
+    const [nextPage, setNextPage] = useState('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1015');
     const [loading, setLoading] = useState(false);
 
     const fetchPokemons = async () => {
@@ -22,7 +22,7 @@ function PokemonList() {
 
                 return {
                     name: detailsData.name,
-                    type: detailsData.types.map(type => type.type.name).join(', '),
+                    type: detailsData.types.map(type => type.type.name),
                     image: detailsData.sprites.front_default,
                 };
             }));
@@ -31,14 +31,21 @@ function PokemonList() {
                 const newPokemons = [...prevPokemons, ...pokemonDetails];
                 const uniquePokemons = Array.from(new Set(newPokemons.map(p => p.name)))
                     .map(name => newPokemons.find(p => p.name === name));
-                return uniquePokemons; // Use find method to filter pokemons withe same name, and avoid duplicates
+                return uniquePokemons; // Use find method to filter pokemons with the same name, and avoid duplicates
             });
+
         } catch (error) {
             console.error('Error fetching Pokémon data:', error);
         } finally {
             setLoading(false);
         }
     };
+
+    const memorizedPokemons = useMemo(() => {
+        const uniquePokemons = Array.from(new Set(pokemons.map(p => p.name)))
+            .map(name => pokemons.find(p => p.name === name));
+        return uniquePokemons;
+    }, [pokemons]);
 
     useEffect(() => {
         fetchPokemons();
@@ -60,19 +67,72 @@ function PokemonList() {
         };
     }, [loading]); // Listen for scroll events
 
+    function getTypeColor(type) {
+        switch (type.toLowerCase()) {
+            case 'grass':
+                return 'grass';
+            case 'fire':
+                return 'fire';
+            case 'water':
+                return 'water';
+            case 'electric':
+                return 'electric';
+            case 'poison':
+                return 'poison';
+            case 'flying':
+                return 'flying';
+            case 'bug':
+                return 'bug';
+            case 'normal':
+                return 'normal';
+            case 'ground':
+                return 'ground';
+            case 'fairy':
+                return 'fairy';
+            case 'fighting':
+                return 'fighting';
+            case 'psychic':
+                return 'psychic';
+            case 'rock':
+                return 'rock';
+            case 'steel':
+                return 'steel';
+            case 'ice':
+                return 'ice';
+            case 'dragon':
+                return 'dragon';
+            case 'dark':
+                return 'dark';
+            case 'ghost':
+                return 'ghost';
+            // Add more cases for other types as needed
+            default:
+                return 'missigno'; // Default color
+        }
+    }
+
     return (
         <div>
             <h1>Pokémon Feed</h1>
             <ul className='pokemon-container'>
-                {pokemons.map((pokemon, index) => (
+                {memorizedPokemons.map((pokemon, index) => (
                     <li key={index} className='pokemon-card'>
                         <Link to={`/pokemon/${pokemon.name}`}>
-                            <div>
+                            <div className="pokemon-img">
                                 <img src={pokemon.image} alt={pokemon.name} />
                             </div>
-                            <div>
-                                <p>Name: {pokemon.name}</p>
-                                <p>Type: {pokemon.type}</p>
+                            <div className="pokemon-body">
+                                <h3>{pokemon.name}</h3>
+                                <div className='pokemon-types'>
+                                    {pokemon.type && pokemon.type.length > 0 ? (
+                                        <>
+                                            <p className={getTypeColor(pokemon.type[0])}><span>{pokemon.type[0]}</span></p>
+                                            {pokemon.type[1] && <p className={getTypeColor(pokemon.type[1])}><span>{pokemon.type[1]}</span></p>}
+                                        </>
+                                    ) : (
+                                        <p style={{ color: getTypeColor('missigno') }}>missigno</p>
+                                    )}
+                                </div>
                             </div>
                         </Link>
                     </li>
